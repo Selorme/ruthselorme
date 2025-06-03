4a4c48cb292aa41ea013555b2e38bfc98a5ce6fa
Ruth Selorme <ruthacolatse.official@gmail.com>
2025-06-03 12:41:32 +0300

    removed the redirects for css through cloudfront

--- app.py
+++ app.py
@@ -112,21 +112,21 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/static/img/<path:filename>')
-def redirect_images(filename):
-    return redirect(f'https://cdn.ruthselormeacolatse.info/static/img/{filename}', code=301)
-
-@app.route('/static/css/<path:filename>')
-def redirect_css(filename):
-    return redirect(f'https://cdn.ruthselormeacolatse.info/static/css/{filename}', code=301)
-
-@app.route('/static/js/<path:filename>')
-def redirect_js(filename):
-    return redirect(f'https://cdn.ruthselormeacolatse.info/static/js/{filename}', code=301)
-
-@app.route('/static/img/<path:filename>')
-def redirect_static_images(filename):
-    return redirect(f'https://cdn.ruthselormeacolatse.info/static/img/{filename}', code=301)
+# @app.route('/static/img/<path:filename>')
+# def redirect_images(filename):
+#     return redirect(f'https://cdn.ruthselormeacolatse.info/static/img/{filename}', code=301)
+#
+# @app.route('/static/css/<path:filename>')
+# def redirect_css(filename):
+#     return redirect(f'https://cdn.ruthselormeacolatse.info/static/css/{filename}', code=301)
+#
+# @app.route('/static/js/<path:filename>')
+# def redirect_js(filename):
+#     return redirect(f'https://cdn.ruthselormeacolatse.info/static/js/{filename}', code=301)
+#
+# @app.route('/static/img/<path:filename>')
+# def redirect_static_images(filename):
+#     return redirect(f'https://cdn.ruthselormeacolatse.info/static/img/{filename}', code=301)
 
 
 @app.errorhandler(404)
